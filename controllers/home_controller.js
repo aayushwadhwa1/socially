7f464499231ef1a3792b075580400284fcf77393
Aayush <Aayush@AAYUSH>
2020-07-14 22:04:23 +0530

    Added controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+// A group of different actions is known as controller
+
+//home is the name of the funciton
+module.exports.home = function(req,res){
+    return res.end('<h1>h1 tag is running on socially</h1>');
+}
