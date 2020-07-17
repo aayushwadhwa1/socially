166c5d2dfc10e48940a3f8c340686902edd564c7
Aayush <aayush13n97@gmail.com>
2020-07-17 23:58:51 +0530

    setup mongoose

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-
+const db = require('./config/mongoose')
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
@@ -14,6 +14,7 @@ app.set('layout extractScripts', true);
 //use express router
 app.use('/', require('./routes'));
 
+//setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
