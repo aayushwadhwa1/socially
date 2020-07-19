3a656b382c70c5bc6d4ead7b0529ed34b73e25f9
Aayush <aayush13n97@gmail.com>
2020-07-19 23:32:56 +0530

    cookie parser added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,7 @@
 //home is the name of the funciton
 module.exports.home = function(req,res){
     // return res.end('<h1>h1 tag is running on socially</h1>');
+    console.log(req.cookies);
     return res.render('home', {
         title: "Home"
     });
