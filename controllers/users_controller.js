7f299cabfbd40550dbe7c9c7af5abcee76276c44
Aayush <aayush13n97@gmail.com>
2020-07-20 01:45:35 +0530

    user sign up and saving details in DB

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res)
 {
     return res.render('user_profile', {
@@ -24,7 +26,30 @@ module.exports.signIn = function(req,res)
 //get the sign up data
 module.exports.create = function(req, res)
 {
-    //TODO later
+    if(req.body.password != req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err)
+        {
+            console.log('User not dound while signing up');
+            return;
+        }
+
+        if(!user)
+        {
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user'); return;}
+
+                res.redirect('/users/sign-in');
+            })
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    })
 }
 
 //sign in and create the session
