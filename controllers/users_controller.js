fc01a826cbc9056adc272f9a91b2f8a433b9918b
Aayush <aayush13n97@gmail.com>
2020-09-21 01:45:58 +0530

    Authorization checks and logout added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,6 +10,9 @@ module.exports.profile = function(req,res)
 //render the sign up page
 module.exports.signUp = function(req,res)
 {
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title: "Socially | Sign Up"
     });
@@ -18,6 +21,9 @@ module.exports.signUp = function(req,res)
 //render the sign in page
 module.exports.signIn = function(req,res)
 {
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title: "Socially | Sign In"
     });
@@ -55,5 +61,11 @@ module.exports.create = function(req, res)
 //sign in and create the session
 module.exports.createSession = function(req, res)
 {
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+
     return res.redirect('/');
 }
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 const passport = require('passport');
 
-router.get('/profile', usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 router.post('/create', usersController.create);
@@ -14,4 +14,7 @@ router.post('/create-session',passport.authenticate(
     'local',
     {failureRedirect: '/users/sign-in'},
 ), usersController.createSession);
+
+router.get('/sign-out', usersController.destroySession);
+
 module.exports = router;
